800489b4ef0945c542d86067376387f6fb8f795f
Lucas <lucassoares2007@hotmail.com>
2023-01-10 14:51:26 -0300

    refactor: indentation

--- src/entities/likes.entities.ts
+++ src/entities/likes.entities.ts
@@ -1,25 +1,20 @@
-import {
-    Column,
-    PrimaryGeneratedColumn,
-    Entity,
-    ManyToOne,
-} from "typeorm";
-import {User} from './user.entities.ts';
-import {Posts} from './post.entities.ts';
-@Entity("likes")
-class Likes{
-    @PrimaryGeneratedColumn("uuid")
-    id:string;
+import { Column, PrimaryGeneratedColumn, Entity, ManyToOne } from 'typeorm';
+import { User } from './user.entities.ts';
+import { Posts } from './post.entities.ts';
 
-    @Column({type:"date"})
-    createdat:string;
-    
-    @ManyToOne(() => User, (user) => user.likes)
+@Entity('likes')
+class Likes {
+  @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+  @Column({ type: 'date' })
+    createdat: string;
+
+  @ManyToOne(() => User, (user) => user.likes)
     user: string;
 
-    @ManyToOne(() => Posts, (posts) => posts.likes)
+  @ManyToOne(() => Posts, (posts) => posts.likes)
     posts: string;
-
 }
 
-export default Likes
+export default Likes;
