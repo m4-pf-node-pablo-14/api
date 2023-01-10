3ad7b460b21fb364f412b64e709e3f0e7c2049cb
Lucas <lucassoares2007@hotmail.com>
2023-01-10 18:49:27 -0300

    build: controllers services routes users and login and remove examples collections

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,15 @@
 import express from 'express';
 import 'express-async-errors';
 import handleError from './errors/handleError';
-import exampleLoginRouter from './routes/examplesLogin.routes';
-import exampleMessageRouter from './routes/examplesMessages.routes';
-import exampleUserRouter from './routes/examplesUsers.routes';
+import loginRouter from './routes/login.routes';
+import userRouter from './routes/users.routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/examples', exampleMessageRouter);
-app.use('/examples', exampleUserRouter);
-app.use('/examples', exampleLoginRouter);
+app.use('/users', userRouter);
+app.use('/login', loginRouter);
 
 app.use(handleError);
 
