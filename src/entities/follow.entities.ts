2bbcf3d358912108fac8107b22d94a8ee810b73f
Vinicius Quirino <106350676+ViniciusQuirino@users.noreply.github.com>
2023-01-10 12:06:31 -0300

    follow.entitie finished

---
+++ src/entities/follow.entities.ts
@@ -0,0 +1,16 @@
+import { Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import User from "./user.entities.ts";
+
+@Entity("follow")
+class Follow {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @ManyToOne(() => User, (user) => user.following)
+  following: User;
+
+  @ManyToOne(() => User, (user) => user.followers)
+  followers: User;
+}
+
+export default Follow;
