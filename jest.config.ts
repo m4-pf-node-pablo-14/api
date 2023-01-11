97fbe74851e8af18bb098ae1fd37dafe325b58a7
Vinicius Quirino <106350676+ViniciusQuirino@users.noreply.github.com>
2023-01-11 17:18:22 -0300

    test/follow finished

--- jest.config.ts
+++ jest.config.ts
@@ -6,5 +6,5 @@
 export default {
   coverageProvider: 'v8',
   preset: 'ts-jest',
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  testMatch: ['**/__tests__/integration/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
 };
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import handleError from './errors/handleError';
 import commentRouter from './routes/comments.routes';
+import followRoutes from './routes/follow.routes';
 import likePostRouter from './routes/likePost.routes';
 import loginRouter from './routes/login.routes';
 import userRouter from './routes/users.routes';
@@ -14,6 +15,7 @@ app.use('/users', userRouter);
 app.use('/login', loginRouter);
 app.use('/comments', commentRouter);
 app.use('/like/post', likePostRouter);
+app.use('/follow',followRoutes)
 
 app.use(handleError);
 
