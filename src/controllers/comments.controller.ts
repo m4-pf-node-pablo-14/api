77de7e5954a31db7d98cd91eb32e3b46fd50ca5d
Pedro Nogueira <pedrophpn2002@hotmail.com>
2023-01-12 15:08:18 -0300

    feat/add deleteComment route

--- src/controllers/comments.controller.ts
+++ src/controllers/comments.controller.ts
@@ -1,5 +1,7 @@
+import { IUserUpdate } from './../interfaces/users.interfaces';
 import { Request, Response } from 'express';
 import createCommentsService from '../services/comments/createComments.service';
+import { deleteCommentService } from '../services/comments/deleteComment.service';
 
 const createCommentsController = async (req: Request, res: Response) => {
   const postId = req.params.id;
@@ -7,4 +9,11 @@ const createCommentsController = async (req: Request, res: Response) => {
   return res.status(201).json(post);
 };
 
-export { createCommentsController };
+const deleteCommentController = async (req: Request, res: Response) => {
+  const commentToDeleteId: string = req.params.id
+  const requesterUserId: string = req.user.id
+  await deleteCommentService(commentToDeleteId, requesterUserId)
+  return res.status(204).json()
+}
+
+export { createCommentsController, deleteCommentController };
--- src/routes/comments.routes.ts
+++ src/routes/comments.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { createCommentsController } from '../controllers/comments.controller';
+import { createCommentsController, deleteCommentController } from '../controllers/comments.controller';
 import ensureAuthMiddleware from '../middlewares/ensureAuth.middleware';
 
 const commentRouter = Router();
 
 commentRouter.post('/:id', ensureAuthMiddleware, createCommentsController);
+commentRouter.delete('/:id', ensureAuthMiddleware, deleteCommentController)
 
 export default commentRouter;
