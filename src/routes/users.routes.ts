a6203caa19ee21bb29746777481e18860f0705d7
Lucas <lucassoares2007@hotmail.com>
2023-01-12 10:30:48 -0300

    build: middleware user is exist

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -10,6 +10,7 @@ import {
 } from '../controllers/users.controllers';
 import ensureAuthMiddleware from '../middlewares/ensureAuth.middleware';
 import ensureDataIsValidMiddleware from '../middlewares/ensureDataIsValid.middleware';
+import ensureUserIsExistMiddleware from '../middlewares/ensureUserIsExist.middleware';
 import ensureUserIsPermitMiddleware from '../middlewares/ensureUserIsPermit.middleware';
 import {
   userSerializer,
@@ -24,23 +25,31 @@ userRouter.post(
   createUserController,
 );
 
-userRouter.get('', ensureAuthMiddleware, listUserController);
+userRouter.get(
+  '',
+  ensureAuthMiddleware,
+  ensureUserIsExistMiddleware,
+  listUserController,
+);
 
 userRouter.get(
   '/comments/:id',
   ensureAuthMiddleware,
+  ensureUserIsExistMiddleware,
   listUserCommentsController,
 );
 
 userRouter.get(
   '/recomendedFollows/:id',
   ensureAuthMiddleware,
+  ensureUserIsExistMiddleware,
   listUsersWithSameFollowerController,
 );
 
 userRouter.get(
   '/postsLiked/:id',
   ensureAuthMiddleware,
+  ensureUserIsExistMiddleware,
   listPostsLikesController,
 );
 
@@ -55,6 +64,7 @@ userRouter.patch(
 userRouter.delete(
   '/:id',
   ensureAuthMiddleware,
+  ensureUserIsExistMiddleware,
   ensureUserIsPermitMiddleware,
   deleteUserController,
 );
