049b4f46f1d7f025674e4a34aac8d03dc36cfc26
Gabriel <drnkgabriel5@gmail.com>
2023-01-12 11:31:10 -0300

    correction of listings

--- src/services/users/listUsersWithSameFollower.service.ts
+++ src/services/users/listUsersWithSameFollower.service.ts
@@ -2,15 +2,14 @@ import AppDataSource from '../../data-source';
 import Follow from '../../entities/follow.entities';
 import User from '../../entities/user.entities';
 
-const listUsersWithSameFollowerService = async (
-  userId: string,
-): Promise<User[]> => {
+const listUsersWithSameFollowerService = async (tokenId: string): Promise<User[]> => {
+
   const userRepository = AppDataSource.getRepository(User);
   const userQueryBuilder = userRepository.createQueryBuilder('users');
 
   const users = await userQueryBuilder
     .leftJoinAndSelect(Follow, 'follow', 'follow.followersId = users.id')
-    .where('follow.followersId = :userId', { userId })
+    .where('follow.followersId = :tokenId', { tokenId })
     .getMany();
 
   return users;
