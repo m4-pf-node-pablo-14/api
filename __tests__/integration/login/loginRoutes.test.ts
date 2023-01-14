4843d25682e9636052b5a9be079a843b92b9a888
Lucas <lucassoares2007@hotmail.com>
2023-01-14 17:38:53 -0300

    refactor: indentation

--- __tests__/integration/login/loginRoutes.test.ts
+++ __tests__/integration/login/loginRoutes.test.ts
@@ -2,7 +2,12 @@ import request from 'supertest';
 import app from '../../../src/app';
 import AppDataSource from '../../../src/data-source';
 import { DataSource } from 'typeorm';
-import { mockedLoginRequest } from '../../mocks';
+import {
+  mockedLoginRequest,
+  mockedLoginRequestTwo,
+  mockedUserRequest,
+  mockedUserRequestTwo,
+} from '../../mocks';
 
 describe('test routes /login', () => {
   let connection: DataSource;
@@ -15,6 +20,17 @@ describe('test routes /login', () => {
       .catch((err) => {
         console.error('Error during Data Source initialization', err);
       });
+
+    await request(app).post('/users').send(mockedUserRequest);
+    const userDelete = await request(app)
+      .post('/users')
+      .send(mockedUserRequestTwo);
+    const tokenDelete = await request(app)
+      .post('/login')
+      .send(mockedLoginRequestTwo);
+    await request(app)
+      .delete(`/users/${userDelete.body.id}`)
+      .set('Authorization', `Bearer ${tokenDelete.body.token}`);
   });
 
   afterAll(async () => {
@@ -24,21 +40,26 @@ describe('test routes /login', () => {
   test('Must be able to login', async () => {
     const response = await request(app).post('/login').send(mockedLoginRequest);
 
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(201);
     expect(response.body).toHaveProperty('token');
   });
 
   test('Should not be able to login with the user with incorrect password or email', async () => {
-    const response = await request(app).post('/login').send(mockedLoginRequest);
+    const response = await request(app).post('/login').send({
+      email: 'teste@teste.com',
+      password: '123456',
+    });
 
     expect(response.status).toBe(404);
     expect(response.body).toHaveProperty('message');
   });
 
   test('Should not be able to login with the user with date in deleteAt', async () => {
-    const response = await request(app).post('/login').send(mockedLoginRequest);
+    const response = await request(app)
+      .post('/login')
+      .send(mockedLoginRequestTwo);
 
-    expect(response.status).toBe(400);
+    expect(response.status).toBe(404);
     expect(response.body).toHaveProperty('message');
   });
 });
