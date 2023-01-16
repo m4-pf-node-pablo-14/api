27c2a525f939ce154129af1bcf0f4a01d3cde2be
unknown <gelanlucas@gmail.com>
2023-01-16 19:07:30 -0300

    fix/admDeleteUser

--- src/services/users/deleteUser.service.ts
+++ src/services/users/deleteUser.service.ts
@@ -3,12 +3,25 @@ import User from '../../entities/user.entities';
 
 const deleteUserService = async (userId: string, req: any): Promise<void> => {
   const userRepository = AppDataSource.getRepository(User);
-  const user = await userRepository.findOneBy({
+  const deletedUser = await userRepository.findOneBy({
     id: userId,
   });
+  
+  const user = await AppDataSource.getRepository(User).findOneBy({
+    id: req.user.id,
+  });
+  const deleted = {
+    ...deletedUser,
+    status: 'Banned'
+  }
+  
+  if (user.isAdm === true) {
+    await userRepository.softRemove(deleted);
+    await userRepository.save(deleted);
+  }
 
-  await userRepository.softRemove(user);
-  await userRepository.save(user);
+  await userRepository.softRemove(deletedUser);
+  await userRepository.save(deletedUser);
 };
 
 export default deleteUserService;
